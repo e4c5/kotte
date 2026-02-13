4be3a1ec64342fe26b0249120d5a645824bd69b2
Raditha Dissanayake <raditha@raditha.com>
2026-02-13 16:13:07 +0000

    Add dev container with Apache AGE installation

---
+++ .devcontainer/init-db.sql
@@ -0,0 +1,5 @@
+-- Load Apache AGE extension on database startup
+CREATE EXTENSION IF NOT EXISTS age;
+
+-- Create a default graph for testing (optional)
+-- SELECT * FROM ag_catalog.create_graph('my_graph');
